caaccf72efedb4dac6d80d091a13451ebd8a15c3
kishan-97 <82228780+kishan-97@users.noreply.github.com>
2021-06-30 16:51:39 +0530

    Add files via upload

--- reg.js
+++ reg.js
@@ -1,4 +1,23 @@
+var username = document.getElementById("username");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var cpassword = document.getElementById("cpassword");
 
-var inp1=getElementById('c').value;
-var inp2=getElementById('d').value;
-alert(inp1);
+var submit = document.getElementById("button");
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  var pass = 0;
+  if (username.length <= 0) {
+    window.alert("Please Enter Username");
+    pass = 1;
+  }
+  if (password.value.length < 8) {
+    window.alert("Weak Password, Password should be of at least 8 letters");
+    pass = 1;
+  }
+  if (password.value !== cpassword.value) {
+    pass = 1;
+    window.alert("Please Enter same password in both fields");
+  }
+  if (pass === 0) window.location.href = "main.html";
+});
